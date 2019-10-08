28b16b591d65e943d3a7878342454c53fb7df5d8
dhyana1984 <xiongyi_1984@foxmail.com>
2019-10-09 00:36:59 +0800

    viewController之间传值

--- Chapter15/UIViewControllerTest2/UIViewControllerTest2/ViewController.swift
+++ Chapter15/UIViewControllerTest2/UIViewControllerTest2/ViewController.swift
@@ -8,16 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ViewControllerTwoProtocol {
 
+    
+
+    var label : UILabel?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //此label用于显示从后面界面传递过来的数据
+        label = UILabel(frame: CGRect(x:20, y:400, width: 300,height: 30))
+        self.view.addSubview(label!)
     }
 
 
     @IBAction func touch(_ sender: Any) {
-        self.present(ViewControllerTwo(), animated: true, completion: nil)
+        //正向传值，通过属性或者构造器
+        //通过属性向ViewControllerTwo传值
+        //let viewController = ViewControllerTwo()
+        //viewController.data = "This data is passed from the first page by property"
+        
+        //通过构造方法向ViewControllerTwo传值
+        //let viewController = ViewControllerTwo(data: "This data is passed from the first page by constructor")
+        
+  
+        let viewController = ViewControllerTwo(data: "This data is passed from the first page by protocol")
+        //设置代理,以便通过代理的回调从后面页面向本页面传值
+        //viewController.delegate = self
+        
+        //对闭包赋值，通过闭包从后面页面向本页面传值
+        viewController.closure = {
+            (data:String) in self.label?.text = data
+        }
+        
+        //present方法是展现另一个viewController
+        self.present(viewController, animated: true, completion: nil)
+    }
+    func sentData(data: String) {
+        self.label?.text = data
     }
 }
 
--- Chapter15/UIViewControllerTest2/UIViewControllerTest2/ViewControllerTwo.swift
+++ Chapter15/UIViewControllerTest2/UIViewControllerTest2/ViewControllerTwo.swift
@@ -1,5 +1,5 @@
 //
-//  ViewControllerTwoViewController.swift
+//  ViewControllerTwo.swift
 //  UIViewControllerTest2
 //
 //  Created by Chris Xiong on 2019/10/8.
@@ -9,22 +9,48 @@
 import UIKit
 
 class ViewControllerTwo: UIViewController {
-
+    var data:String?
+    //optional类型代理属性，遵守ViewControllerTwoProtocol协议
+    var delegate:ViewControllerTwoProtocol?
+    var closure: ((String) -> Void)?
+    init(data: String){
+        self.data = data
+        super.init(nibName:nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+  
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.red
+        let button = UIButton(frame: CGRect(x:100,y:200,width: 100,height:30))
+        button.setTitle("返回", for: UIControl.State())
+        button.addTarget(self, action: #selector(ret), for: UIControl.Event.touchUpInside)
+        self.view.addSubview(button)
+        let label = UILabel(frame: CGRect(x:10,y:300,width: 300, height: 30))
+        label.text = data
+        label.textColor = UIColor.white
+        self.view.addSubview(label)
+    }
+    @objc func ret(){
+        //反向传值，通过协议或者闭包
+        //通过代理属性回调协议定义的方法从后向前传值
+        //delegate?.sentData(data: "The data from the second page by protocol")
+        //通过闭包从后向前传值
+        self.closure!("The data from the second page by closure")
+        //dismiss和present是对应的一对方法，dismiss是返回present的页面
+        self.dismiss(animated: true, completion: nil)
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
+}
+
+protocol ViewControllerTwoProtocol {
+    func sentData(data:String)
 }
