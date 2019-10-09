13fc4a63759b52c3ca2cab4ce6f8bef106ddf630
dhyana1984 <xiongyi_1984@foxmail.com>
2019-10-09 23:32:26 +0800

    导航视图控制器

---
+++ Chapter15/UINavigationControllerTest/UINavigationControllerTest/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  UINavigationControllerTest
+//
+//  Created by Chris Xiong on 2019/10/9.
+//  Copyright © 2019 org.chris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = UIColor.white
+        self.title = "第2个视图控制器"
+    }
+    
+
+
+
+}
