30503b7067499e61138a0130a01859d8a359ba90
dhyana1984 <xiongyi_1984@foxmail.com>
2019-10-27 02:02:03 +0800

    记事本列表，数据持久化

---
+++ Chapter18/NoteBook/NoteBook/DataManager.swift
@@ -0,0 +1,13 @@
+//
+//  DataManager.swift
+//  NoteBook
+//
+//  Created by Chris Xiong on 2019/10/26.
+//  Copyright © 2019 org.chris. All rights reserved.
+//
+
+import UIKit
+
+class DataManager: NSObject {
+
+}
---
+++ Chapter18/NoteBook/NoteBook/NoteModel.swift
@@ -0,0 +1,13 @@
+//
+//  NoteModel.swift
+//  NoteBook
+//
+//  Created by Chris Xiong on 2019/10/27.
+//  Copyright © 2019 org.chris. All rights reserved.
+//
+
+import UIKit
+
+class NoteModel: NSObject {
+
+}
